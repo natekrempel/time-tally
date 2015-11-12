64f02b66115833f37900d6b33eb44e4f929bfbc6
Nate Krempel <nate@wcst.com>
2015-11-12 15:41:55 -0600

    organized and commented

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ var Datastore = require('nedb'),
     moment = require('moment'),
     db = {};
 
+/**
+ * Tally up how long you work on a project with a simple gulp task
+ * @class
+ */
 function TimeTally() {
     this.user = gitConfig.sync().user.email;
     this.packageName = JSON.parse(fs.readFileSync("./package.json")).name;
-}
 
-TimeTally.prototype.init = function() {
-
+    // Set up local datastore for time logs
     db = new Datastore({
         filename: './time-logs/'+this.user+'.json',
         timestampData: true
     });
 
     db.loadDatabase();
-
-    this.startTimer();
 
     // Hijack stdin so the program will not close instantly
     process.stdin.resume();
@@ -28,55 +28,71 @@ TimeTally.prototype.init = function() {
 
     // Grab uncaught exceptions
     process.on('uncaughtException', this.endTimer.bind(this));
+}
 
-    // Do something when app is closing - Can't think of scenerios when this is needed
-    // process.on('exit', this.exitHandler.bind(this,{cleanup:true}));
-
-};
-
-TimeTally.prototype.startTimer = function () {
-    console.log(" ===== START TIMER =====");
-    // get current time in file and append to it
-
+/**
+ * Start the timer / create a new log
+ * @memberOf TimeTally
+ * @callback {function} cb - callback to allow gulp to move onto the next task. This ensures the timer starts first always
+ */
+TimeTally.prototype.startTimer = function (cb) {
     var newLog = {
-            "projectName": this.packageName,
-            "startTime": new Date()
-        };
+        "projectName": this.packageName,
+        "startTime": new Date()
+    };
 
     db.insert(newLog, function(){
-        console.log(" ===== NEW LOG ADDED =====");
+        console.log(" ===== START TIMER =====");
+        cb();
     });
 };
 
+/**
+ * Stop the timer / add the current time and total duration for the last log
+ * @memberOf TimeTally
+ * @param {object} err - catches an error if the process is not ended with 'SIGINT' or by a 'uncaughtException'
+ */
 TimeTally.prototype.endTimer = function (err) {
-    console.log(" ===== END TIMER =====");
-    var newDate = new Date();
-    if (err) console.log(err.stack);
-    db.find({endDate: {$exists: false}}, function(err, docs){
-        var start = moment(docs[0].startTime),
-            end = moment(newDate),
-            total = end.diff(start),
-            sessionDuration = moment.duration(total);
+    var newDate = new Date(),
+        end = moment(newDate);
 
-        db.update({endDate: {$exists: false}}, {$set: {endDate: newDate, duration: total}}, {}, function(err, numReplaced, newDoc){
-            console.log("Session Duration - ");
-            console.log(sessionDuration._data.hours+"hrs", sessionDuration._data.minutes+"min", sessionDuration._data.seconds+"s");
+    if (err) console.log(err.stack);
+
+    db.find({endTime: {$exists: false}}, function(err, docs){
+
+        var start = moment(docs[0].startTime),
+            total = end.diff(start),
+            sessDur = moment.duration(total),
+            endTimeLog = {
+                endTime: newDate,
+                duration: total
+            };
+
+        db.update({endTime: {$exists: false}}, {$set: endTimeLog}, {}, function(err, numReplaced, newDoc){
+            console.log(" ===== END TIMER =====");
+            console.log("Session Duration - "+sessDur._data.hours+"hrs", sessDur._data.minutes+"min", sessDur._data.seconds+"s");
             process.exit();
         });
     });
 
 };
 
+/**
+ * Log out to the CLI the total of all duration fields for the project
+ * @memberOf TimeTally
+ */
 TimeTally.prototype.total = function () {
-    db.loadDatabase();
+    var total = 0,
+        i;
 
+    db.loadDatabase();
     db.find({duration: {$exists: true}}, function(err, docs){
-        var total = 0,
-            i;
+
         for (i = 0; i < docs.length; i++) {
             console.log(docs[i]._id, moment.duration(docs[i].duration)._data.hours);
             total += docs[i].duration;
         }
+
         total = moment.duration(total);
         // Log out Time for the entirety of the project
         console.log(total._data.hours+"hrs", total._data.minutes+"min", total._data.seconds+"s");
@@ -84,14 +100,22 @@ TimeTally.prototype.total = function () {
     });
 };
 
+/**
+ * Log out to the CLI the total of all duration fields for the current day
+ * @memberOf TimeTally
+ */
 TimeTally.prototype.dayTotal = function () {
+    var today = new Date(),
+        total = 0,
+        i;
+
     db.loadDatabase();
-
-    var currentDate = moment().toDate().toJSON();
-    db.find({duration: {$exists: true}, $where: function(){ return moment(this.startTime).isSame(new Date(), 'day');}}, function(err, docs){
-        var total = 0,
-            i;
-
+    db.find({
+        duration: {$exists: true},
+        $where: function(){
+            return moment(this.startTime).isSame(new Date(), 'day');
+        }
+    }, function(err, docs){
         for (i = 0; i < docs.length; i++) {
             total += docs[i].duration;
         }
